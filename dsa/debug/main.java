03d4d379dd2933f4a886c756b6e5818771a1fa1e
Nihal Machhi <nihalmachhi11@gmail.com>
2026-06-30 08:01:48 +0530

    Fix palindrome logic

--- dsa/debug/main.java
+++ dsa/debug/main.java
@@ -1,10 +1,24 @@
-// package dsa.debug;
+package dsa.debug;
 
 import java.util.*;
 
 public class main {
     public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int userinput = scn.nextInt();
+        int originalInput = userinput;
+        int reverse = 0;
 
+        while (userinput > 0) {
+            int digit = userinput % 10;
+            reverse = reverse * 10 + digit;
+            userinput /= 10;
+        }
+        if (originalInput == reverse) {
+            System.out.println("given number is palindrome");
+        } else {
+            System.out.println("given number is not a palindrome");
+        }
     }
 
 }
